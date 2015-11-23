9d02a4a175341ac464fdc361b4ba9f5b2404d7a2
Ineentho <hk.henrik@gmail.com>
2015-11-23 17:23:58 +0100

    Don't hardcode hostnames

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,7 @@ if (document.querySelector('#player')) {
     var name = document.querySelector('#player').getAttribute('streamer');
 
     jwplayer('player').setup({
-        file: 'rtmp://home.gigavoid.com:1935/live/' + name,
+        file: 'rtmp://' + location.hostname + ':1935/live/' + name,
         image: '/static/imgs/gvsurgedefault.png',
         width: '100%',
         aspectratio: '16:9',
